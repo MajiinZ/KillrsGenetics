b6ce49d194b959d8a6b32d8aad0f7ffef36c36d3
zakfa <chefnayton@gmail.com>
2026-09-02 23:09:00 -0700

    this app shoul work on any device

--- shared/src/commonMain/kotlin/org/mz/killrs/shared/Constants.kt
+++ shared/src/commonMain/kotlin/org/mz/killrs/shared/Constants.kt
@@ -5,7 +5,7 @@ package org.mz.killrs.shared
 object Constants {
     const val SERVER_PORT = 8080
 
-    const val WEB_CLIENT_ID = "623171350158-ovv1k4dtn8eisimjorm2ir57kgjao59s.apps.googleusercontent.com"
+    const val WEB_CLIENT_ID = "623171350158-ua9j5hm633ff73gqlk8tehbil5h9jr8l.apps.googleusercontent.com"
 
     const val MAX_QUANTITY = 10
     const val MIN_QUANTITY = 1
